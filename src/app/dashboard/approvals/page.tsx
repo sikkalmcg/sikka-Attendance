4078813acb48f52a05793b8cc202d6c4339de41f
Ajay Somra <sikkalmcg@gmail.com>
2026-09-01 15:49:04 +0530

    Initial Commit

--- src/app/dashboard/approvals/page.tsx
+++ src/app/dashboard/approvals/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { differenceInCalendarDays } from "date-fns";
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo, useEffect, useCallback } from "react";
 import { 
   Table, 
   TableHeader, 
